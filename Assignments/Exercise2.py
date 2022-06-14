f1e5ad990323c2c05efef4639397b3def2dcf86b
Ali <alissatrujillo23@gmail.com>
2022-06-14 12:53:40 -0400

    Create Exercise2.py

---
+++ Assignments/Exercise2.py
@@ -0,0 +1,25 @@
+# DSC 530
+# Week 2 Assignment: Python Review
+# Author: Alissa Trujillo
+# June 14, 2022
+
+def main():
+    print("Hello World! I wonder why that is always the default coding text to start with")
+    print(22 + 33)
+    print(999 - 777)
+    print(11 * 7)
+    print(666 / 3)
+    print("Hello my name is" + "Alissa")
+
+    about_me = ["Alissa", 26, "Sacramento", "Grad Student"]
+    print(about_me)
+
+    about_me.append("June 23")
+    print(about_me)
+
+    favorite_bands = ("Glass Animals", "The 1975", "LANY", "Louis the Child")
+    print(favorite_bands)
+
+
+if __name__ == "__main__":
+    main()
